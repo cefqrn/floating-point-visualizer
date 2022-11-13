6d12fc8f008f32664ee2430ed5acd6ec7011b9d8
Cefqrn <cefqrn@gmail.com>
2022-11-13 01:32:30 -0500

    added ability to change values using the textboxes

--- float.js
+++ float.js
@@ -1,3 +1,9 @@
+const BIT_MASKS = {
+  SIGN:     0x80000000,
+  EXPONENT: 0x7f800000,
+  MANTISSA: 0x007fffff
+}
+
 export class Float {
   constructor() {
     // js converts numbers to 32-bit signed ints before doing bitwise operations
@@ -21,26 +27,65 @@ export class Float {
     this.bits = parseInt(params.get("v") ?? "0", 16)
   }
 
+  get negative() {
+    return (this.bits & BIT_MASKS.SIGN) >> 31
+  }
+
   get sign() {
-    return (this.bits & 0x80000000) ? -1 : 1
+    return  (this.bits & BIT_MASKS.SIGN) ? -1 : 1
   }
 
   get exponent() {
-    return ((this.bits & 0x7f800000) >> 23) - 127
+    return ((this.bits & BIT_MASKS.EXPONENT) >> 23) - 127
+  }
+
+  set exponent(newValue) {
+    const val = Number.parseFloat(newValue)
+
+    this.bits &= BIT_MASKS.SIGN | BIT_MASKS.MANTISSA
+    this.bits |= ((val + 127) << 23) & BIT_MASKS.EXPONENT
   }
 
   get mantissa() {
-    return ((this.bits & 0x007fffff) | 0x00800000) * Math.pow(2, -23)
+    return ((this.bits & BIT_MASKS.MANTISSA) | 0x00800000) * Math.pow(2, -23)
+  }
+
+  set mantissa(newValue) {
+    const val = Number.parseFloat(newValue)
+
+    if (isNaN(val)) return
+
+    const man = Math.round(val * Math.pow(2, 23))
+    this.bits &= ~BIT_MASKS.MANTISSA
+    this.bits |= man & BIT_MASKS.MANTISSA
   }
 
   get value() {
-    const exp = this.exponent
     if (this.exponent == 128) {
-      return (this.bits & 0x007fffff) ? NaN : Infinity * this.sign
-    } else if (!(this.bits & 0x7fffffff)) {
+      return (this.bits & BIT_MASKS.MANTISSA) ? NaN : Infinity * this.sign
+    } else if (!(this.bits & ~BIT_MASKS.SIGN)) {
       return 0
     }
 
     return this.sign * this.mantissa * Math.pow(2, this.exponent)
+  }
+
+  set value(newValue) {
+    const val = Number.parseFloat(newValue)
+
+    if (isNaN(val)) {
+      this.bits = BIT_MASKS.EXPONENT | BIT_MASKS.MANTISSA
+      return
+    }
+
+    const exp = Math.floor(Math.log2(val))
+    const man = Math.round((val/Math.pow(2, exp - 23)))
+    const sgn = val < 0 || Object.is(val, -0) ? 1 : 0
+
+    if (exp >= 128) {
+      this.bits = (sgn << 31) | BIT_MASKS.EXPONENT
+    } else {
+      this.bits = (sgn << 31) | ((exp + 127) << 23) | (man & BIT_MASKS.MANTISSA)
+    }
   }
 }
--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 import { Float } from "./float.js"
 
 addEventListener("DOMContentLoaded", _ => {
+  const ELEMENT_IDS = {
+    LINK:     0b00001,
+    VALUE:    0b00010,
+    SIGN:     0b00100,
+    EXPONENT: 0b01000,
+    MANTISSA: 0b10000,
+    ALL:      0b11111
+  }
+
   const floatValue = new Float
 
   const linkElement = document.getElementById("permalink")
-  function updatePermalink() {
+  function updateLink() {
     const urlParams = new URLSearchParams({"v": floatValue.encode()})
     const url = `${window.location.origin}${window.location.pathname}?${urlParams}`
 
@@ -12,21 +21,37 @@ addEventListener("DOMContentLoaded", _ => {
     linkElement.innerText = url
   }
 
-  const valElement = document.getElementById("val")
-  const sgnElement = document.getElementById("sgn")
-  const expElement = document.getElementById("exp")
-  const manElement = document.getElementById("man")
-  function updateValues() {
+  const valElement = document.getElementById("val")  
+  function updateValue() {
     const value = floatValue.value
     if (value === 0) {
-      valElement.value = `${floatValue.sign === 1 ? "" : "-"}${value}`
+      valElement.value = `${floatValue.negative ? "-" : ""}${value}`
     } else {
       valElement.value = value
     }
-    
+  }
+
+  const sgnElement = document.getElementById("sgn")
+  function updateSign() {
     sgnElement.value = floatValue.sign === 1 ? "+" : "-"
+  }
+
+  const expElement = document.getElementById("exp")
+  function updateExponent() {
     expElement.value = floatValue.exponent
+  }
+
+  const manElement = document.getElementById("man")
+  function updateMantissa() {
     manElement.value = floatValue.mantissa
+  }
+
+  function updateElements(selectedElements) {
+    if (selectedElements & ELEMENT_IDS.LINK    ) updateLink()
+    if (selectedElements & ELEMENT_IDS.VALUE   ) updateValue()
+    if (selectedElements & ELEMENT_IDS.SIGN    ) updateSign()
+    if (selectedElements & ELEMENT_IDS.EXPONENT) updateExponent()
+    if (selectedElements & ELEMENT_IDS.MANTISSA) updateMantissa()
   }
 
   
@@ -36,19 +61,44 @@ addEventListener("DOMContentLoaded", _ => {
 
   checkboxes.reverse()
 
+  function updateCheckboxes() {
+    for (let i=0; i < checkboxes.length; ++i) {
+      checkboxes[i].checked = floatValue.getBit(i)
+    }
+  }
+
   floatValue.load()
   for (let i=0; i < checkboxes.length; ++i) {
     const checkbox = checkboxes[i]
 
     checkbox.checked = floatValue.getBit(i)
 
-    checkbox.addEventListener("change", _ => {
+    checkbox.addEventListener("click", _ => {
       floatValue.setBit(i, checkbox.checked)
-      updateValues()
-      updatePermalink()
+      updateElements(ELEMENT_IDS.ALL)
     })
   }
 
-  updateValues()
-  updatePermalink()
+  valElement.addEventListener("input", _ => {
+    floatValue.value = valElement.value
+
+    updateCheckboxes()
+    updateElements(ELEMENT_IDS.ALL & ~(ELEMENT_IDS.VALUE))
+  })
+
+  expElement.addEventListener("input", _ => {
+    floatValue.exponent = expElement.value
+
+    updateCheckboxes()
+    updateElements(ELEMENT_IDS.ALL & ~(ELEMENT_IDS.EXPONENT))
+  })
+
+  manElement.addEventListener("input", _ => {
+    floatValue.mantissa = manElement.value
+
+    updateCheckboxes()
+    updateElements(ELEMENT_IDS.ALL & ~(ELEMENT_IDS.MANTISSA))
+  })
+
+  updateElements(ELEMENT_IDS.ALL)
 })
