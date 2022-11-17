b0f85169538348e4d86f1073d5ef8eab6d35a4ff
Cefqrn <cefqrn@gmail.com>
2022-11-17 01:09:30 -0500

    made checkbox navigation only possible with arrow keys and `a` and `d`

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,36 +1,60 @@
-document.onkeydown = function(event) {
-  const focus = document.activeElement
-  const parent = focus.parentElement
+let checkboxes
 
-  if (parent.className !== "checkbox-container") {
+let focusIndex
+let focusNext
+let focusPrev
+
+{
+  let selectedIndex = 0
+
+  focusIndex = newIndex => {
+    checkboxes[selectedIndex].tabIndex = -1
+    checkboxes[newIndex].tabIndex = 0
+    checkboxes[newIndex].focus()
+    selectedIndex = newIndex
+  }
+  
+  focusNext = () => {
+    focusIndex((selectedIndex + 1) % checkboxes.length)
+  }
+  
+  focusPrev = () => {
+    focusIndex((selectedIndex ? selectedIndex : checkboxes.length) - 1)
+  }
+}
+
+document.onkeydown = event => {
+  if (document.activeElement.parentElement?.className !== "checkbox-container") {
     return
   }
 
-  let newFocus
   switch (event.key) {
+    case "a":
+    case "A":
     case "ArrowLeft":
-      if (parent.previousElementSibling) {
-        newFocus = parent.previousElementSibling
-      } else if (parent.parentElement.previousElementSibling) {
-        newFocus = parent.parentElement.previousElementSibling.lastElementChild
-      }
-
+      focusPrev()
       break
+    case "d":
+    case "D":
     case "ArrowRight":
-      if (parent.nextElementSibling) {
-        newFocus = parent.nextElementSibling
-      } else if (parent.parentElement.nextElementSibling) {
-        newFocus = parent.parentElement.nextElementSibling.firstElementChild
-      }
-
+      focusNext()
       break
     default:
       return
   }
 
-  if (newFocus && newFocus.className === "checkbox-container") {
-    newFocus.focus()
-  }
-
   event.preventDefault()
 }
+
+addEventListener("DOMContentLoaded", _ => {
+  checkboxes = Array.from(document.getElementsByClassName("checkbox-container")).map(
+    (element) => element.firstElementChild
+  )
+
+  for (let i=1; i < checkboxes.length; ++i) {
+    checkboxes[i].tabIndex = -1
+    checkboxes[i].addEventListener("click", _ => {
+      focusIndex(i)
+    })
+  }
+})
